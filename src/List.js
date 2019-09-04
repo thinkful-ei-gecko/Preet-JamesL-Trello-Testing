4a37ff0e5cf82aa25fb854f1b548daf78aea6239
James Lee <jameswlee109@gmail.com>
2019-09-04 11:44:52 -0500

    finished snapshot test

--- src/List.js
+++ src/List.js
@@ -3,12 +3,14 @@ import Card from './Card'
 import './List.css';
 
 export default function List(props) {
+  console.log(props);
   return (
     <section className='List'>
       <header className='List-header'>
         <h2>{props.header}</h2>
       </header>
       <div className='List-cards'>
+        
         {props.cards.map((card) =>
           <Card
             key={card.id}
